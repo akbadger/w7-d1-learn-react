ef9163e1645ec5fbbda133273040f92d1d8dcd4a
Aubrey Badger <akbadger@gmail.com>
2017-03-14 09:43:19 -0400

    added category select input and background color

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -2,12 +2,23 @@ import React from 'react'
 
 class AddTodo extends React.Component {
     render() {
-        return      <div className="input-group">
+        return  <div>
+                    <select className="form-control">
+                        <option>Select Category...</option>
+                        <option>Work</option>
+                        <option>Bills</option>
+                        <option>Personal</option>
+                        <option>Projects</option>
+                        <option>Chores</option>
+                    </select>
+                    <br/>
+                    <div className="input-group">
                         <input type="text" className="form-control" placeholder="What do you need to do?"/>
                         <span className="input-group-btn">
                             <button className="btn btn-default" type="button">Add</button>
                         </span>
                     </div>
+            </div>
     }
 }
 
